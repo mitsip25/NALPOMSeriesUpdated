fa5428c198e41410fed32ab6ecc0d91ae6e94df0
unknown <mitsip25@gmail.com>
2020-02-17 10:35:31 -0800

    created deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,12 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage{
+	
+	
+	public DealsPage(){
+		System.out.println("deals page const....");
+	}
+
+}

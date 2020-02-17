90b8ba2c0d29c6f28fa741f86d271f1f8c902bf4
unknown <mitsip25@gmail.com>
2020-02-17 12:40:15 -0800

    added Deals Page test cases in DealsPageTest class.

---
+++ src/test/java/com/qa/hubspot/testcases/DealsPageTest.java
@@ -0,0 +1,15 @@
+package com.qa.hubspot.testcases;
+
+public class DealsPageTest {
+
+
+
+	public void setUp(){
+		System.out.println("setup method...");
+	}
+
+	public void getDealsPageTitle(){
+		System.out.println("Deals Page title....");
+	}
+	
+}
